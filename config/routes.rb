1e9049fc5aad650565a8d11a30a18e7f68381d51
divyap1 <1divyap1@gmail.com>
2017-07-15 16:58:35 +1200

    Remove temporary data sets

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
   get 'home/index' => 'home#index'
-
-  # Data end points for different graphs
-  get 'home/basic' => 'home#basic', :defaults => { :format => 'json' }
-  get 'home/filters' => 'home#filters', :defaults => { :format => 'json' }
-  get 'home/motion' => 'home#motion', :defaults => { :format => 'json' }
-
   root 'home#index'
 end
