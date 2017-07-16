6a52bc9892996705123781412de9c1cdd996ce92
divyap1 <1divyap1@gmail.com>
2017-07-16 17:17:34 +1200

    Remove irrelevant countries from world map

--- app/services/populate_database.rb
+++ app/services/populate_database.rb
@@ -31,4 +31,7 @@ class PopulateDatabase
   # labour force female and male % of female/male pop
   # gender equality index - bubble graph
   # DO SOMETHING ABOUT UNEMPLOYMENT + PAY GAP!
+
+# Antartica removed from map
+# {"type":"MultiPolygon","id":11,"arcs":[[[43]],[[44]],[[45]],[[46]],[[47]],[[48]],[[49]],[[50]],[[51]],[[52]],[[53]],[[54]],[[55]],[[56]],[[57]],[[58]],[[59]],[[60]],[[61]],[[62]],[[63]],[[64]],[[65]],[[66]],[[67]],[[68]],[[69]],[[70]],[[71]],[[72]],[[73]],[[74]],[[75]],[[76]],[[77]],[[78]],[[79]],[[80]],[[81]],[[82]],[[83]],[[84]],[[85]]],"properties":{"admin":"Antarctica","id":"ATA"}},
 end
