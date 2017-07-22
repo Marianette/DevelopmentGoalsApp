6bb8ccecdb3a0f76863f9491866a16af7189146e
divyap1 <1divyap1@gmail.com>
2017-07-22 23:21:01 +1200

    Move functions that need to be loaded after DOM into separate js file

--- app/assets/javascripts/education_and_employment.js
+++ app/assets/javascripts/education_and_employment.js
@@ -221,52 +221,6 @@ function getMessage(d){
   return name + "</br>" + value;
 }
 
-// React to change events on filters
-$(function(){
-  // React to indicator selection change
-  $("#select-map-dataset").on("change", function(){
-    if(this.value != selectedDataset) {
-      selectedDataset = this.value;
-      // disable irrelevant filter options
-      if (selectedDataset == "comparison") {
-        $("#map-gender-parity").attr("disabled", true);
-        // ensure that unavailable filter is not being applied to dataset
-        if(selectedFilter == "diff") {
-          selectedFilter = "female";
-          $("#map-female").prop("checked", true);
-        }
-      } else {
-        $("#map-gender-parity").attr("disabled", false);
-      }
-      // if changing data set, choose most recent year for new data setAnimataion
-      currentYearIndex = yearArrays[selectedDataset].length - 1;
-
-      // Need to change slider values
-      var years = yearArrays[selectedDataset];
-      $('#years-selector').attr("min", d3.min(years));
-      $('#years-selector').attr("max", d3.max(years));
-
-      applyFilter();
-    }
-  });
-
-  // React to data selection change
-  $("#map-gender-filter-selection input").on("change", function(){
-    var value = $('input[name=gender-select]:checked', '#map-gender-filter-selection').val();
-    if(value != selectedFilter){
-      selectedFilter = value;
-      applyFilter();
-    }
-  });
-
-  // React to year slider change
-  $("#year-slider").on("change", function(){
-    var newYear = $("#years-selector").val();
-    currentYearIndex = snapToValidYear(newYear);
-    applyFilter();
-  });
-});
-
 function applyFilter(){
   updateYearViews();
   updateMapTitleAndInfo(selectedDataset, selectedFilter);
--- app/assets/javascripts/material.js
+++ app/assets/javascripts/material.js
@@ -2,3 +2,51 @@ $(function() {
   $('.button-collapse').sideNav();
   $('select').material_select();
 });
+
+// Other functions that need to be defined after the DOM is completely loaded
+
+// React to change events on map filters
+$(function(){
+  // React to indicator selection change
+  $("#select-map-dataset").on("change", function(){
+    if(this.value != selectedDataset) {
+      selectedDataset = this.value;
+      // disable irrelevant filter options
+      if (selectedDataset == "comparison") {
+        $("#map-gender-parity").attr("disabled", true);
+        // ensure that unavailable filter is not being applied to dataset
+        if(selectedFilter == "diff") {
+          selectedFilter = "female";
+          $("#map-female").prop("checked", true);
+        }
+      } else {
+        $("#map-gender-parity").attr("disabled", false);
+      }
+      // if changing data set, choose most recent year for new data setAnimataion
+      currentYearIndex = yearArrays[selectedDataset].length - 1;
+
+      // Need to change slider values
+      var years = yearArrays[selectedDataset];
+      $('#years-selector').attr("min", d3.min(years));
+      $('#years-selector').attr("max", d3.max(years));
+
+      applyFilter();
+    }
+  });
+
+  // React to data selection change
+  $("#map-gender-filter-selection input").on("change", function(){
+    var value = $('input[name=gender-select]:checked', '#map-gender-filter-selection').val();
+    if(value != selectedFilter){
+      selectedFilter = value;
+      applyFilter();
+    }
+  });
+
+  // React to year slider change
+  $("#year-slider").on("change", function(){
+    var newYear = $("#years-selector").val();
+    currentYearIndex = snapToValidYear(newYear);
+    applyFilter();
+  });
+});
