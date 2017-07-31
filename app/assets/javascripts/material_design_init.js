71e34255857b6dac841223b988682027fddf18ed
divyap1 <1divyap1@gmail.com>
2017-07-31 20:38:39 +1200

    Add hover effects on icons in index page

--- app/assets/javascripts/material_design_init.js
+++ app/assets/javascripts/material_design_init.js
@@ -7,3 +7,15 @@ $(function() {
 function clearVis() {
   $('.content-container').remove();
 }
+
+$(function() {
+  $('.index-icons').on('mouseenter', function(){
+    var id = "#" + this.id + "label";
+    $(id).css("display", "block");
+  });
+
+  $('.index-icons').on('mouseleave', function(){
+    var id = "#" + this.id + "label";
+    $(id).css("display", "none");
+  });
+});
